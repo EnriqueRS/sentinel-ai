27ff2ebe5e7660cb4a52c079cad286dff4e16a42
EnriqueRS <enrique.rios.santos@gmail.com>
2025-12-11 14:01:48 +0100

    init commit

---
+++ src/test/java/com/github/enriquers/sentinelai/SentinelAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.enriquers.sentinelai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelAiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
